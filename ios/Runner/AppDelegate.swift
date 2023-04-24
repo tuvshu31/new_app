313e1c0837b0c2dc06cba7c80357d23a339ea844
tuvshu31 <97154380+tuvshu31@users.noreply.github.com>
2023-04-24 22:54:48 +0800

    ..

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -18,16 +18,8 @@ import awesome_notifications
    // This function registers the desired plugins to be used within a notification background action
       SwiftAwesomeNotificationsPlugin.setPluginRegistrantCallback { registry in          
           SwiftAwesomeNotificationsPlugin.register(
-<<<<<<< Updated upstream
+
             with: registry.registrar(forPlugin: "io.flutter.plugins.awesomenotifications.AwesomeNotificationsPlugin")!)
-=======
-<<<<<<< HEAD
-            with: registry.registrar(forPlugin: "io.flutter.plugins.awesomenotifications.AwesomeNotificationsPlugin")!)          
-    
-=======
-            with: registry.registrar(forPlugin: "io.flutter.plugins.awesomenotifications.AwesomeNotificationsPlugin")!)
->>>>>>> c8f933f1b6bbde55b60cbe62eae1fb11eaa804ea
->>>>>>> Stashed changes
       }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
