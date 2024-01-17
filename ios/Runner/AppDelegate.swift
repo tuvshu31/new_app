9faedbaaa4594c368b634f10dd12b3cf91456b40
tuvshu31 <97154380+tuvshu31@users.noreply.github.com>
2024-01-17 21:19:29 +0800

    ..

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,8 +2,6 @@ import UIKit
 import Flutter
 import GoogleMaps
 import FirebaseCore
-import awesome_notifications
-import shared_preferences_foundation
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -15,9 +13,9 @@ import shared_preferences_foundation
     //Google Maps Setup
     GMSServices.provideAPIKey("AIzaSyAHTYs2cMm87YH3wppr6wTtKRZxfyXjvB4")
     GeneratedPluginRegistrant.register(with: self)
-  // if #available(iOS 10.0, *) {
-  //       UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
-  //     }
+  if #available(iOS 10.0, *) {
+        UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+      }
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
