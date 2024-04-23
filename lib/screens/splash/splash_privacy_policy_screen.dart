fb4f8e1f64e1a385d3bce3b34c5b7344a6aec1b6
tuvshu31 <97154380+tuvshu31@users.noreply.github.com>
2024-04-23 13:44:12 +0800

    ...

--- lib/screens/splash/splash_privacy_policy_screen.dart
+++ lib/screens/splash/splash_privacy_policy_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-// import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class SplashPrivacyPolicyScreen extends StatefulWidget {
   const SplashPrivacyPolicyScreen({super.key});
@@ -25,22 +25,22 @@ class _SplashPrivacyPolicyScreenState extends State<SplashPrivacyPolicyScreen> {
       customActions: Container(),
       body: SizedBox(
         height: Get.height,
-        // child: WebViewWidget(
-        //   gestureRecognizers: gestureRecognizers,
-        //   controller: WebViewController()
-        //     ..setJavaScriptMode(JavaScriptMode.unrestricted)
-        //     ..setNavigationDelegate(
-        //       NavigationDelegate(
-        //         onProgress: (int progress) {},
-        //         onPageStarted: (String url) {},
-        //         onPageFinished: (String url) {},
-        //         onWebResourceError: (WebResourceError error) {},
-        //       ),
-        //     )
-        //     ..loadRequest(
-        //       Uri.parse('https://sites.google.com/view/erdenet24/home'),
-        //     ),
-        // ),
+        child: WebViewWidget(
+          gestureRecognizers: gestureRecognizers,
+          controller: WebViewController()
+            ..setJavaScriptMode(JavaScriptMode.unrestricted)
+            ..setNavigationDelegate(
+              NavigationDelegate(
+                onProgress: (int progress) {},
+                onPageStarted: (String url) {},
+                onPageFinished: (String url) {},
+                onWebResourceError: (WebResourceError error) {},
+              ),
+            )
+            ..loadRequest(
+              Uri.parse('https://sites.google.com/view/erdenet24/home'),
+            ),
+        ),
       ),
     );
   }
